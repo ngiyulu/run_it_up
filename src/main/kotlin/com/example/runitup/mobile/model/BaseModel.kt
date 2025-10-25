fae72f79401c2fcdda98be494635b3ea2b91cdbc
Christian Ngiyulu <cngiyulu@gmail.com>
2025-10-25 16:44:58 -0500

    fixed code for approving waivers

--- src/main/kotlin/com/example/runitup/mobile/model/BaseModel.kt
+++ src/main/kotlin/com/example/runitup/mobile/model/BaseModel.kt
@@ -8,7 +8,7 @@ import java.time.LocalDate
 open class BaseModel(
     open var createdAt: LocalDate = LocalDate.now(),
     var updatedAt:Long = 0,
-    private var version:Int = 1,
+    var version:Int = 1,
     @field:JsonProperty("isActive") // used for deserialization (field/ctor param)
     @get:JsonProperty("isActive")   // used for serialization (getter)
     @Field("isActive")              // stored as "isActive" in Mongo
