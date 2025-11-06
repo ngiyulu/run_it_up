852c9022ec2ecdf37077e45a413259294417a29a
Christian Ngiyulu <cngiyulu@gmail.com>
2025-11-06 16:48:02 -0600

    typo

--- src/main/kotlin/com/example/runitup/mobile/rest/v1/dto/session/StartSessionModel.kt
+++ src/main/kotlin/com/example/runitup/mobile/rest/v1/dto/session/StartSessionModel.kt
@@ -1,3 +1,3 @@
 package com.example.runitup.mobile.rest.v1.dto.session
 
-class StartSessionModel(val sessionId: String, var overrideMinimum:Boolean, val isAdmin:Boolean  =false)
+class StartSessionModel(val sessionId: String)
