a73e402bc72fc0193534a1db4d973bf1245b67f7
Christian Ngiyulu <cngiyulu@gmail.com>
2025-11-07 17:57:47 -0600

    updated

--- src/main/kotlin/com/example/runitup/mobile/rest/v1/controllers/runsession/CancelBookingController.kt
+++ src/main/kotlin/com/example/runitup/mobile/rest/v1/controllers/runsession/CancelBookingController.kt
@@ -1,9 +1,11 @@
 package com.example.runitup.mobile.rest.v1.controllers.runsession
 
+import com.example.runitup.common.model.AdminUser
 import com.example.runitup.mobile.model.RunSession
 import com.example.runitup.mobile.repository.service.BookingDbService
 import com.example.runitup.mobile.rest.v1.controllers.BaseController
 import com.example.runitup.mobile.rest.v1.dto.session.CancelSessionModel
+import com.example.runitup.mobile.security.UserPrincipal
 import com.example.runitup.mobile.service.LeaveSessionService
 import com.example.runitup.mobile.service.RunSessionService
 import com.example.runitup.web.security.AdminPrincipal
@@ -25,8 +27,15 @@ class CancelBookingController: BaseController<CancelSessionModel, RunSession>() 
 
     override fun execute(request: CancelSessionModel): RunSession {
         val auth = SecurityContextHolder.getContext().authentication
-        val savedAdmin = auth.principal as AdminPrincipal
-        val run = leaveSessionService.execute(request, savedAdmin.admin)
+        val savedPrincipal = auth.principal
+        if (savedPrincipal is AdminPrincipal){
+          return complete(request, savedPrincipal.admin)
+        }
+        return complete(request, null)
+    }
+
+    private fun complete(request: CancelSessionModel, admin: AdminUser?): RunSession{
+        val run = leaveSessionService.execute(request, admin)
         run.updateStatus()
         // we need to return this for admin
         run.bookings = run.bookings.map {
