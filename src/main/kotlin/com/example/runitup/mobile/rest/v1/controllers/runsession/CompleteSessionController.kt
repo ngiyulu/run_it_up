d50355ea42bfb63df83f68a0c72078312811f1fb
Christian Ngiyulu <cngiyulu@gmail.com>
2025-11-08 11:59:24 -0600

    fixed logic completed jobs

--- src/main/kotlin/com/example/runitup/mobile/rest/v1/controllers/runsession/CompleteSessionController.kt
+++ src/main/kotlin/com/example/runitup/mobile/rest/v1/controllers/runsession/CompleteSessionController.kt
@@ -3,6 +3,7 @@ package com.example.runitup.mobile.rest.v1.controllers.runsession
 import com.example.runitup.mobile.enum.RunStatus
 import com.example.runitup.mobile.exception.ApiRequestException
 import com.example.runitup.mobile.model.RunSession
+import com.example.runitup.mobile.repository.service.BookingDbService
 import com.example.runitup.mobile.rest.v1.controllers.BaseController
 import com.example.runitup.mobile.rest.v1.dto.session.ConfirmSessionModel
 import com.example.runitup.mobile.service.RunSessionService
@@ -13,6 +14,7 @@ class CompleteSessionController: BaseController<ConfirmSessionModel, RunSession>
 
 
     lateinit var runSessionService: RunSessionService
+    lateinit var bookingDbService: BookingDbService
 
     override fun execute(request: ConfirmSessionModel): RunSession {
         var run = cacheManager.getRunSession(request.sessionId) ?: throw ApiRequestException(text("invalid_session_id"))
@@ -20,6 +22,7 @@ class CompleteSessionController: BaseController<ConfirmSessionModel, RunSession>
             throw  ApiRequestException(text("invalid_session_cancel"))
         }
         run.status = RunStatus.COMPLETED
+        bookingDbService.completeAllBooking(run.id.orEmpty())
         run = runSessionService.updateRunSession(run)
         return run
 
