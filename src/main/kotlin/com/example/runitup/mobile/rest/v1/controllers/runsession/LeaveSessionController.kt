232650212448d351dffad4d8d4361e2f675a8110
Christian Ngiyulu <cngiyulu@gmail.com>
2025-11-08 11:43:53 -0600

    fixed leave session issue

--- src/main/kotlin/com/example/runitup/mobile/rest/v1/controllers/runsession/LeaveSessionController.kt
+++ src/main/kotlin/com/example/runitup/mobile/rest/v1/controllers/runsession/LeaveSessionController.kt
@@ -4,9 +4,11 @@ import com.example.runitup.mobile.exception.ApiRequestException
 import com.example.runitup.mobile.model.RunSession
 import com.example.runitup.mobile.rest.v1.controllers.BaseController
 import com.example.runitup.mobile.rest.v1.dto.session.CancelSessionModel
+import com.example.runitup.mobile.security.UserPrincipal
 import com.example.runitup.mobile.service.LeaveSessionService
 import com.example.runitup.mobile.service.RunSessionService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
 
 @Service
@@ -21,7 +23,8 @@ class LeaveSessionController: BaseController<CancelSessionModel, RunSession>() {
 
 
     override fun execute(request: CancelSessionModel): RunSession {
-        val user = cacheManager.getUser(request.userId)?: throw ApiRequestException("user_not_found")
+        val auth = SecurityContextHolder.getContext().authentication.principal as UserPrincipal
+        val user = cacheManager.getUser(auth.id.orEmpty()) ?: throw ApiRequestException(text("user_not_found"))
         val run = leaveSessionService.cancelBooking(user, request.sessionId)
         return runService.updateRunSession(run)
     }
