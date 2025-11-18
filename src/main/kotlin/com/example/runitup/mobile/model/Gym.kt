8fd0664a04d8ce5b332d3263614a106c163e1ddb
Christian Ngiyulu <cngiyulu@gmail.com>
2025-11-18 00:55:02 -0600

    fixed code cor creating gym dfualt constructor

--- src/main/kotlin/com/example/runitup/mobile/model/Gym.kt
+++ src/main/kotlin/com/example/runitup/mobile/model/Gym.kt
@@ -4,21 +4,21 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint
 
 class Gym (
-    @Id var id: String,
-    var line1: String,
-    var line2: String,
-    var location: GeoJsonPoint?,
-    var zoneid:String,
-    var image:String?,
-    var fee: Double,
-    var phoneNumber: String?,
-    var city: String,
-    var title: String,
-    var state: String,
-    var notes: String,
-    var description: String,
+    @Id var id: String = "",
+    var line1: String= "",
+    var line2: String = "",
+    var location: GeoJsonPoint? = null,
+    var zoneid:String = "",
+    var image:String? = null,
+    var fee: Double = 0.0,
+    var phoneNumber: String? = null,
+    var city: String = "",
+    var title: String = "",
+    var state: String ="",
+    var notes: String = "",
+    var description: String = "",
     var country:String = "USA",
-    var zipCode:String
+    var zipCode:String = ""
 ): BaseModel(){
     fun getAddress():String{
         return "$line1, $line2, $city, $state $zipCode"
