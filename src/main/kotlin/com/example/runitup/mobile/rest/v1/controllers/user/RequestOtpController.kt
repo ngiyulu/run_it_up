750a64987b8f3a4a4bd134f2bdcd77f4805d12da
Christian Ngiyulu <cngiyulu@gmail.com>
2025-11-25 23:25:29 -0600

    fixed code for otp

--- src/main/kotlin/com/example/runitup/mobile/rest/v1/controllers/user/RequestOtpController.kt
+++ src/main/kotlin/com/example/runitup/mobile/rest/v1/controllers/user/RequestOtpController.kt
@@ -1,5 +1,6 @@
 package com.example.runitup.mobile.rest.v1.controllers.user
 
+import com.example.runitup.mobile.repository.UserRepository
 import com.example.runitup.mobile.repository.service.OtpDbService
 import com.example.runitup.mobile.rest.v1.controllers.BaseController
 import com.example.runitup.mobile.rest.v1.dto.OtpResponse
@@ -11,8 +12,13 @@ import org.springframework.stereotype.Service
 class RequestOtpController:  BaseController<SendOtpRequest, OtpResponse>() {
     @Autowired
     lateinit var otpDbService: OtpDbService
+
+    @Autowired
+    lateinit var userRepository: UserRepository
+
     override fun execute(request: SendOtpRequest): OtpResponse {
-        val otp = otpDbService.generateOtp(null, request.phoneNumber)
+        val user = userRepository.findByPhone(request.phoneNumber)
+        val otp = otpDbService.generateOtp(user?.id, request.phoneNumber)
         print(otp)
         return OtpResponse(true)
     }
