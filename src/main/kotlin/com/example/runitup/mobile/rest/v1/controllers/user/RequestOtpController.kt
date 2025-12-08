597cf2def606a61f1f71f9ecbc3df93d33487eae
Christian Ngiyulu <cngiyulu@gmail.com>
2025-12-08 17:58:55 -0600

    adding sms code

--- src/main/kotlin/com/example/runitup/mobile/rest/v1/controllers/user/RequestOtpController.kt
+++ src/main/kotlin/com/example/runitup/mobile/rest/v1/controllers/user/RequestOtpController.kt
@@ -1,10 +1,14 @@
 package com.example.runitup.mobile.rest.v1.controllers.user
 
+import com.example.runitup.mobile.clicksend.SmsService
 import com.example.runitup.mobile.repository.UserRepository
 import com.example.runitup.mobile.repository.service.OtpDbService
 import com.example.runitup.mobile.rest.v1.controllers.BaseController
 import com.example.runitup.mobile.rest.v1.dto.OtpResponse
 import com.example.runitup.mobile.rest.v1.dto.SendOtpRequest
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -16,10 +20,17 @@ class RequestOtpController:  BaseController<SendOtpRequest, OtpResponse>() {
     @Autowired
     lateinit var userRepository: UserRepository
 
+    @Autowired
+    lateinit var smsService: SmsService
+
     override fun execute(request: SendOtpRequest): OtpResponse {
         val user = userRepository.findByPhone(request.phoneNumber)
         val otp = otpDbService.generateOtp(user?.id, request.phoneNumber)
-        print(otp)
+        logger.info("otp object = $otp")
+        runBlocking {
+            val response = smsService.sendSmsDetailed(otp.phoneNumber, "Your runitup Otp code is ${otp.code}. Do not share it with anyone")
+            logger.info("sms response for ${request.phoneNumber} = $response")
+        }
         return OtpResponse(true)
     }
 
